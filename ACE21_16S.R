78ff7bd1621d415f81f8a17625ef7658aeda6980
Shayle Matsuda <shaylematsuda@gmail.com>
2021-03-31 08:15:43 -1000

    16s

--- ACE21_16S.R
+++ ACE21_16S.R
@@ -40,9 +40,6 @@ tax1 <- as.matrix(TAX[, -1], dimnames = list(TAX$OTU, colnames(TAX[-1])))
 rownames(tax1) <- tax0$OTU
 tax <- tax_table(tax1)
 
-#Tell phyloseq what is what otu and tax tables need to be matrix
-OTU = otu_table(OTU3, taxa_are_rows = T)
-TAX = tax_table(TAX2)
 
 # Read the data into phyloseq
 Bac.seq = phyloseq(otu, tax,sam) #THIS WORKS
@@ -52,8 +49,56 @@ save(Bac.seq, file = "Data/Raw_bacteria_phyloseq.RData")
 
 
 ## check out data
-ntaxa(physeq)  #num taxa
-nsamples(physeq)   #num samples
-sample_names(physeq)[1:300] #samp names
+ntaxa(Bac.seq)  #num taxa
+nsamples(Bac.seq)   #num samples
+sample_names(Bac.seq)[1:300] #samp names
 # sampsy<-sample_names(physeq)[1:300]
 #write.csv(sampsy,"sampsy.csv")
+
+
+####################
+#not used here but great tutorial with images https://vaulot.github.io/tutorials/Phyloseq_tutorial.html
+
+#load in 3000 rare instead
+#read in sample data
+MetaData16<-read.csv("Data/Meta16s_3k.csv") #run in physio script first
+MetaData16$Parent.ID<-as.factor(as.character(MetaData16$Parent.ID))
+MetaData16$Tank.num<-as.factor(as.character(MetaData16$Tank.num))
+
+sam0 <- MetaData16
+sam1 <- as.matrix(sam0[, -1])
+rownames(sam1) <- sam0$sample_name
+sam <- sample_data(data.frame(sam1))
+
+#load in OUT:raw_abundanceTable_100.shared
+
+OTU3k<-read.table("Data/TT17_3000_summer_16S-pipeline_outputs/abundance_table_100_shared.csv", sep=',', header=T)
+
+#from its  
+#otu
+otu3k1 <- as.matrix(OTU3k[, -1])
+rownames(otu3k1) <- OTU3k$sample_name
+otu <- otu_table(otu3k1, taxa_are_rows = FALSE)
+
+
+#tax table annotations_100.taxonomy.csv
+TAX<- read.csv("Data/TT17_3000_summer_16S-pipeline_outputs/annotations_100.taxonomy.csv", colClasses = "character")
+tax1 <- as.matrix(TAX[, -1], dimnames = list(TAX$OTU, colnames(TAX[-1])))
+rownames(tax1) <- TAX$OTU
+tax <- tax_table(tax1)
+
+
+# Read the data into phyloseq
+Bac.seq = phyloseq(otu, tax,sam) #THIS WORKS
+Bac.seq
+
+save(Bac.seq, file = "Data/Raw_bacteria_phyloseq.RData")
+
+
+## check out data
+ntaxa(Bac.seq)  #num taxa
+nsamples(Bac.seq)   #num samples
+sample_names(Bac.seq)[1:300] #samp names
+ #sampsy<-sample_names(physeq)[1:350]
+#write.csv(sampsy,"sampsy.csv")
+
